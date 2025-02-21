64444562ecc5aa6759d17c368cd731f46a5efedc
krishnas05 <kanavkrishna05@gmail.com>
2025-02-21 18:34:22 +0530

    add

--- src/routes/bfhlRoutes.js
+++ src/routes/bfhlRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // Root route with message and operation code
 router.get("/", (req, res) => {
   res.status(200).json({
-    message: "Welcome to the Bajaj Finserv Health API! Use the /bfhl route for POST requests.",
+    message: "Welcome to the Bajaj Finserv Health API by Krishna Sharma! Use the /bfhl route for POST requests.",
     operation_code: 1,
   });
 });
